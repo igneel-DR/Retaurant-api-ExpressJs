03bf1988617ed0d33fc95998f4c4b9613a7fb36e
Hicham <bleus332@gmail.com>
2025-02-28 18:42:09 +0000

    Reccete CRUD

--- Routes/Reccete.js
+++ Routes/Reccete.js
@@ -7,24 +7,54 @@ router.get("/",(req,res)=>{
     res.send("Reccete route is displaying data") 
 })
 
-router.get("/all",(req,res)=>{ 
-    res.send("Chefs route is displaying data") 
+router.get("/all", async (req,res)=>{ 
+    try {
+        const reccetes = await recceteModel.find({});
+        res.json(reccetes)
+    }
+    catch(error){
+        res.json({message: error.message})
+    }
 })
 
-router.get("/names",(req,res)=>{ 
-    res.send("Chefs route is displaying data") 
+router.get("/names", async (req,res)=>{ 
+    try {
+        const reccetes = await recceteModel.find({}, {_id: 0}).select('nom');
+        res.json(reccetes)
+    }
+    catch(error){
+        res.json({message: error.message})
+    }
 })
 
-router.post("/add",(req,res)=>{ 
-    res.send("Chefs route is displaying data") 
+router.post("/add", async(req,res)=>{ 
+    try {
+        const date = await recceteModel.insertOne(req.body);
+        res.json(date)
+    }
+    catch(error){
+        res.json({message: error.message})
+    }
 })
 
-router.put("/update/:name",(req,res)=>{ 
-    res.send("Chefs route is displaying data") 
+router.put("/update/:name", async (req,res)=>{ 
+    try {
+        const date = await recceteModel.updateOne({nom: req.params.name}, req.body);
+        res.json(date)
+    }
+    catch(error){
+        res.json({message: error.message})
+    }
 })
 
-router.delete("/update/:name",(req,res)=>{ 
-    res.send("Chefs route is displaying data") 
+router.delete("/delete/:name", async (req,res)=>{ 
+    try {
+        const date = await recceteModel.deleteOne({nom: req.params.name});
+        res.json(date)
+    }
+    catch(error){
+        res.json({message: error.message})
+    }
 })
 
 module.exports=router;
